4ef02657e372f5d3a13cbc1756ff79719b0f711e
Becky Heaton <becky.a.heaton@outlook.com>
2024-09-26 00:28:42 +1000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// JavaScript code to add an interactive behavior to the button
+
+// Select the button element by its ID
+document.getElementById("alertButton").addEventListener("click", function() {
+    // When the button is clicked, an alert box will pop up with this message
+    alert("Button clicked!");
+});
